1c044ee8e67836151a1e77374724a68b985855ee
BLACK STAR <206607968+blackstarvip@users.noreply.github.com>
2026-03-09 17:44:59 +0500

    Update queue_manager.py

--- queue_manager.py
+++ queue_manager.py
@@ -6,22 +6,25 @@ task_queue = Queue()
 
 print("QUEUE MANAGER LOADED", flush=True)
 def worker():
+    print("WORKER STARTED", flush=True)
 
     while True:
-
         login,password = task_queue.get()
 
-        login_student(login,password)
+        print("QUEUE RECEIVED:", login, flush=True)
+
+        result = login_student(login,password)
+
+        print("LOGIN RESULT:", result, flush=True)
 
         task_queue.task_done()
 
 for i in range(3):
-
     t = Thread(target=worker)
-
     t.daemon = True
-
     t.start()
+
+print("WORKERS INITIALIZED", flush=True)
 
 
 def add_task(login,password):
